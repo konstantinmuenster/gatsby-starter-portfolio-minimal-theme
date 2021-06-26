1adc0c6ae715839a166481e9acb2e66254a88860
Konstantin <konstantin.muenster@mail.com>
2021-06-26 11:47:45 +0200

    Add ArticlesSection to example-site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {
   AboutSection,
+  ArticlesSection,
   ContactSection,
   HeroSection,
   InterestsSection,
@@ -15,6 +16,7 @@ export default function IndexPage() {
       <Seo title="Gatsby Starter for Portfolio Minimal" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
+        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
         <AboutSection sectionId="about" heading="About Portfolio Minimal" />
         <InterestsSection sectionId="details" heading="Details" />
         <ProjectsSection sectionId="features" heading="Built-in Features" />
